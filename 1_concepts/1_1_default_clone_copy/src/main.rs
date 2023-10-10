5995c1ed66d672e874f5e325ef7ebc7e955abdf8
igor udot <horw7001@gmail.com>
2023-10-10 19:02:46 +0300

    update: create point and polyline structs

--- 1_concepts/1_1_default_clone_copy/src/main.rs
+++ 1_concepts/1_1_default_clone_copy/src/main.rs
@@ -1,3 +1,24 @@
+use std::default;
+
+#[derive(Default,Debug, Clone, Copy)]
+struct Point{
+    x:i32,
+    y:i32
+}
+
+#[derive(Debug, Clone)]
+struct Polyline{
+    line: Vec<Point>
+}
+
 fn main() {
-    println!("Implement me!");
+    let p1: Point = Default::default();
+    let p2: Point = Point { x: 1, y: 2 };
+    let pl = Polyline{line:vec![
+        p1,
+        p1.clone(),
+        p2.clone()
+        ]
+    };
+    println!("{:?}", pl.clone());
 }
